b6dd7f1c4fbf5b75abee1344a24c01fb08d86e00
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2023-09-20 23:36:05 +0100

    dix 7

--- models/user.py
+++ models/user.py
@@ -12,8 +12,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String
+    first_na
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
