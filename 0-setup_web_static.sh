0abe666ce59d003bd39f52350f109ebfe3d51a51
ZAKARIA <zakariaelaro@gmail.com>
2023-10-10 03:57:58 -0700

    Task 0 trying to fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-# Setup ur own nginx config
-
-# update and check inginx file
+# install Nginx make folders
 apt-get update
 apt-get install nginx -y
+sudo mkdir -p /data /data/web_static/
 
-# Make folders
-mkdir -p /data/web_static/shared/test
-mkdir -p /data/web_static/shared/
-echo "Welcome Everybody" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/currect
-chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-# RESTART NGNIX
-service nginx start
+sudo mkdir -p /data/web_static/releases/test/ 
+sudo mkdir -p /data/web_static/shared/
+echo "Welcome Everybody" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# restart nginx
+sudo service nginx restart
