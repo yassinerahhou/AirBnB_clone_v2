0a2b3cc2433b5135ebd507aac887255282045cdf
ZAKARIA <zakariaelaro@gmail.com>
2023-10-10 03:43:57 -0700

    Task 0 check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# install Nginx if not already installed and creat new folder
-sudo su
+# Setup ur own nginx config
+
+# update and check inginx file
 apt-get update
 apt-get install nginx -y
-mkdir -p /data /data/web_static/
 
-mkdir -p /data/web_static/releases/test/ 
+# Make folders
+mkdir -p /data/web_static/shared/test
 mkdir -p /data/web_static/shared/
-echo "test hh test hh test hh" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-service nginx restart
-=======
-# sets up the web servers for the deployment of web_static
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
->>>>>>> a671dcd3b702e54e393b2bfc78c4e71a932813d2
+echo "Welcome Everybody" >> /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/currect
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# RESTART NGNIX
+service nginx start
