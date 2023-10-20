5029897d95fc99b0b72543ea3fecdecab9f8ec04
ZAKARIA <zakariaelaro@gmail.com>
2023-10-20 15:57:45 -0700

    trying to fix task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+# task 0
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
+# fun that run xd
 def hello_world():
     return "Hello HBNB!"
 
