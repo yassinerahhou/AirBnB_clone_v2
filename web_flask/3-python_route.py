b973fe9d1abaf5e287a06c0954d17f9c4222ba2a
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 08:01:55 -0700

    Chec ttask 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""C is fun"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',strict_slashes=False)
+def hello_world():
+    """first route"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb',strict_slashes=False)
+def hello_world_hbnb():
+    """s route"""
+    return "HBNB"
+
+@app.route('/c/<text>',strict_slashes=False)
+def text_hbnb(text):
+    """route with varable"""
+    text = text.replace('_', ' ')
+    return "C" + text
+
+@app.route(/python/<text>,strict_slashes=False)
+def python_text(text):
+	"""python route"""
+        text_python = text.replace('_',' ')
+	return "Python" + text_python
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
