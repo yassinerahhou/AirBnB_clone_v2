2c66c7abede30a7e851339207c5e808d42cab726
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 07:51:43 -0700

    Task 2 check

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_world():
     """first route"""
     return "Hello HBNB!"
