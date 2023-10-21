2273caaed23708acab2ede0e2be4f7f283bdd05a
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 07:36:31 -0700

    Task 2 check

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
+"""C is fun"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
+    """first route"""
     return "Hello HBNB!"
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hello_world_hbnb():
+    """s route"""
     return "HBNB"
 
-@app.route('/c/<text>',strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def text_hbnb(text):
-    return "C"
+    """route with varable"""
+    return "C {text}"
+
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
