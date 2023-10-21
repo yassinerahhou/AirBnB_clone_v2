c4fc4994c18a280716a85a446bf8ff844b303aa3
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 12:50:11 -0700

    Check task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """first route"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_world_hbnb():
     """s route"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def text_hbnb(text):
@@ -20,12 +23,14 @@ def text_hbnb(text):
     text = text.replace('_', ' ')
     return "C " + text
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """python route"""
     text = text.replace('_', ' ')
     return "Python {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
