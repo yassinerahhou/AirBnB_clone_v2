bc4f2f9ebf1bf153bb94a616f8debaa800047d5b
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 07:13:49 -0700

    Check task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_world():
 """ route it / """
     return "Hello HBNB!"
