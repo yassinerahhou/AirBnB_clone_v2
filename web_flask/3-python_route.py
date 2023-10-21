260a19e54916e532bf61304553a0bafbde87ce83
ZAKARIA <zakariaelaro@gmail.com>
2023-10-21 08:06:08 -0700

    TASK TRYING TO FIX

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,27 +4,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_world():
     """first route"""
     return "Hello HBNB!"
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hello_world_hbnb():
     """s route"""
     return "HBNB"
 
-@app.route('/c/<text>',strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def text_hbnb(text):
     """route with varable"""
     text = text.replace('_', ' ')
     return "C" + text
 
-@app.route(/python/<text>,strict_slashes=False)
+
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-	"""python route"""
-        text_python = text.replace('_',' ')
-	return "Python" + text_python
+    """python route"""
+    text_python = text.replace('_', ' ')
+    return "Python" + text_python
+
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
