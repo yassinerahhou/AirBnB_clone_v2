a78bdb4f2a40f6c3f7f6729e6406ab87d4584c60
ZAKARIA <zakariaelaro@gmail.com>
2023-10-22 01:47:43 -0700

    Task 5 is fun

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# make instance
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """return HBNB!"""
+    return 'HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def dispaly_hbnb():
+    """display hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_varable():
+    """varable function"""
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>')
+def n_number(n):
+    """display n_number"""
+    return f'{n}'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_templatee_n(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
